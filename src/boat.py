2bc2dd66e4fb6227b18252517e0eefb1bba3a384
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-08-28 14:47:32 -0400

    Random placing

--- src/boat.py
+++ src/boat.py
@@ -1,3 +1,5 @@
+import random
+
 class Boat:
 
     DIR_DOWN = 0
@@ -7,6 +9,8 @@ class Boat:
 
     _DIR_X = [0, 0, -1, 1]
     _DIR_Y = [1, -1, 0, 0]
+
+    MAX_RANDOM_PLACING = 100
 
     def __init__(self, size):
         self.size = size
@@ -26,7 +30,7 @@ class Boat:
             x += dif_x
             y += dif_y
 
-    def place(self, grid, x, y, dir):
+    def _placeIfPossible(self, grid, x, y, dir):
         pos_x = x
         dif_x = Boat._DIR_X[dir]
         pos_y = y
@@ -37,12 +41,28 @@ class Boat:
                 remaining -= 1
                 pos_x += dif_x
                 pos_y += dif_y
-            if remaining == 0:
-                self._place(grid, x, y, dir)
-                return True
+                if remaining == 0:
+                    self._place(grid, x, y, dir)
+                    return True
         except:
             return False
         return False
+
+    def _placeRandomly(self, grid):
+        step = 0
+        while step < Boat.MAX_RANDOM_PLACING:
+            x = random.randint(0, grid.width - 1)
+            y = random.randint(0, grid.width - 1)
+            dir = random.randint(Boat.DIR_DOWN, Boat.DIR_RIGHT)
+            if self._placeIfPossible(grid, x, y, dir):
+                return True
+            step += 1
+        return False
+
+    def place(self, grid, x = -1, y = -1, dir = -1):
+        if x == -1 and y == -1 and dir == -1:
+            return self._placeRandomly(grid)
+        return self._placeIfPossible(grid, x, y, dir)
 
     def hit(self, x, y):
         remaining = len(self.pos_x)
